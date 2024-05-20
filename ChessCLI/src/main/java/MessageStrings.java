473b5932aea34cf872b96264544b33848b3f3750
Dexter21MD <dexter.krzeminski@gmail.com>
2024-05-20 13:37:26 +0200

    Added new welcome player strings

--- ChessCLI/src/main/java/MessageStrings.java
+++ ChessCLI/src/main/java/MessageStrings.java
@@ -1,5 +1,6 @@
 public class MessageStrings
 {
+    String greeting = "Hello";
     String introductionMessage = "Welcome to my little chess game! I hope you will enjoy your time playing it :) ";
-    String introductionOMessagePickOption = "test";
+    String introductionOMessagePickOption = "To start a game pick option 1 or 2 if you want to exit";
 }
