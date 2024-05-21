435fde05a3833b60095309b0757c283740e343f5
Dexter21MD <dexter.krzeminski@gmail.com>
2024-05-21 18:05:36 +0200

    Fixed typo

--- ChessCLI/src/main/java/MessageStrings.java
+++ ChessCLI/src/main/java/MessageStrings.java
@@ -4,5 +4,5 @@ public class MessageStrings
     String introductionMessage = "Welcome to my little chess game! I hope you will enjoy your time playing it :) ";
     String introductionOMessagePickOption = "To start a game pick option 1 or 2 if you want to exit";
     String startGameOption = "Start";
-    String exittGameOption = "Exit";
+    String exitGameOption = "Exit";
 }
